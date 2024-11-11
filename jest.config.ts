8c8956aac5cce371ebd6621529c19ba3b0e97e08
Jumi <feliciastrandberg@gmail.com>
2024-11-11 23:36:04 +0100

    Adds setupTests file and ref. in config

--- jest.config.ts
+++ jest.config.ts
@@ -4,8 +4,14 @@ const config: Config = {
     preset: "ts-jest",
     testEnvironment: "jest-environment-jsdom",
     transform: {
-        "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
-    }
-}
+        "^.+\\.(js|jsx|ts|tsx)$": "ts-jest", 
+    },
+    globals: {
+        'ts-jest': {
+            tsconfig: 'tsconfig.app.json', 
+        },
+    },
+    setupFilesAfterEnv: ['<rootDir>/setupTests.ts'], 
+};
 
 export default config;
