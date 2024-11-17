ea6c3e0731dd720a202dc0a7e09560f958a17edf
Jumi <feliciastrandberg@gmail.com>
2024-11-17 18:26:19 +0100

    Wraps Navbar component in BrowserRouter to pass test

--- src/components/__tests__/Navbar.test.tsx
+++ src/components/__tests__/Navbar.test.tsx
@@ -1,8 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import Navbar from "../Navbar";
+import { BrowserRouter } from "react-router-dom";
 
 test("renders Navbar", () => {
-  render(<Navbar />);
+  render(
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
   const linkElement = screen.getByText(/Home/i);
   expect(linkElement).toBeInTheDocument();
 });
