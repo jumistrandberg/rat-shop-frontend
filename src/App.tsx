1191cbebfe1b8ce813d006a82a93c4580801e7ae
Jumi <feliciastrandberg@gmail.com>
2024-11-17 18:20:56 +0100

    Imports pages and sets routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ShopCategory from "./pages/ShopCategory";
+
 
 function App() {
   return (
@@ -9,8 +14,13 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />   
-          <Route path="/" element={<Home />} />   
+          <Route path="/" element={<Home />} />
+          <Route path="/rats" element={<ShopCategory category="rats" />} />
+          <Route path="/maybe" element={<ShopCategory category="maybe" />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/product/:productId" element={<Product />} />
         </Routes>
       </BrowserRouter>
     </>
